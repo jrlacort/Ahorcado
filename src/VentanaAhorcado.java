e1d7df0a24d4736651fb18d385e38183ef981d81
Admin <Admin@MSI>
2015-01-14 14:22:01 +0100

    He modificado los comentarios

--- src/VentanaAhorcado.java
+++ src/VentanaAhorcado.java
@@ -13,15 +13,14 @@ import javax.swing.JButton;
  */
 public class VentanaAhorcado extends javax.swing.JFrame {
 
-    /**
-     * Creates new form VentanaAhorcado
-     */
+   
     public VentanaAhorcado() {
         initComponents();
     }
     
     private void chequeaBoton(JButton botonPulsado){
         botonPulsado.setVisible(false);
+        
     }
     /**
      * This method is called from within the constructor to initialize the form.
